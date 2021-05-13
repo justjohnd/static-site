0f7607444deee2b6bb8ee73203ab9995ce1bf354
John Denlinger <justjohnd@gmail.com>
2021-05-13 14:13:38 +0900

    Change gallery carousel id name

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
-import bootstrap from 'bootstrap';
+// import bootstrap from 'bootstrap';
 import ScrollOut from 'scroll-out';
 
 import renderGalleryItem from './helpers/randomImg.js';
 
-//Add random unsplash images for development
+// //Add random unsplash images for development
 renderGalleryItem(
   'js-random-img-1',
   'js-random-img-2',
@@ -12,8 +12,34 @@ renderGalleryItem(
 );
 
 //Confirm jquery is loaded (if necessary, delete for production)
-console.log($);
+// console.log($);
 
+//This is for the Scroll Out package to properly function. This package makes elements appear on the screen when they come into view on scroll
+ScrollOut({
+  threshold: 0.2,
+  once: true,
+});
+
+/*
+	Story by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+// (function ($) {
+//   var $window = $(window),
+//     $body = $('body'),
+//     $wrapper = $('#wrapper');
+
+//   // Play initial animations on page load.
+//   $window.on('load', function () {
+//     window.setTimeout(function () {
+//       $body.removeClass('is-preload');
+//     }, 100);
+//   });
+// })(jQuery);
+
+//Waypoints. This code is used to change navbar on scroll past banner.
 const waypoint = new Waypoint({
   element: document.getElementById('js-navbar'),
   handler: function () {
@@ -28,7 +54,30 @@ const waypoint = new Waypoint({
   },
 });
 
-ScrollOut({
-  threshold: 0.2,
-  once: true,
+// Multiple Carousel slides
+$('#carousel-multi-image').on('slide.bs.carousel', function (e) {
+  /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  console.log(idx);
+  var itemsPerSlide = 4;
+  var totalItems = $('#carousel-multi-image .carousel-item').length;
+
+  if (idx >= totalItems - (itemsPerSlide - 1)) {
+    var it = itemsPerSlide - (totalItems - idx);
+    for (var i = 0; i < it; i++) {
+      // append slides to end
+      if (e.direction == 'left') {
+        $('#carousel-multi-image .carousel-item')
+          .eq(i)
+          .appendTo('#carousel-multi-image .carousel-inner');
+      } else {
+        $('#carousel-multi-image .carousel-item')
+          .eq(0)
+          .appendTo('#carousel-multi-image .carousel-inner');
+      }
+    }
+  }
 });
